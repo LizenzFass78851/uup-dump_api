1bc46dbcc9cecbe3076b8d4ac79e4f4f432b1cda
LizenzFass78851 <82592556+LizenzFass78851@users.noreply.github.com>
2025-08-30 08:46:41 +0200

    Update files
    from https://git.uupdump.net/uup-dump/api
    commit https://git.uupdump.net/uup-dump/api/commit/2d2321b21c94691d7d6062eefe8c0653f98ea213

--- shared/main.php
+++ shared/main.php
@@ -16,7 +16,7 @@ limitations under the License.
 */
 
 function uupApiVersion() {
-    return '1.49.0';
+    return "I can't be bothered to update the version; use commit hash.";
 }
 
 require_once dirname(__FILE__).'/auths.php';
