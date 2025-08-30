0a0c55596fe53f788194a3944b7f8adc65377fa3
LizenzFass78851 <82592556+LizenzFass78851@users.noreply.github.com>
2025-08-30 08:52:40 +0200

    Update main.php (own adjustment)
    Revert version number to previous latest version number

--- shared/main.php
+++ shared/main.php
@@ -16,7 +16,7 @@ limitations under the License.
 */
 
 function uupApiVersion() {
-    return "I can't be bothered to update the version; use commit hash.";
+    return "1.49.0";
 }
 
 require_once dirname(__FILE__).'/auths.php';
