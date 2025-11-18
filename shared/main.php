48d0cd6d4f5251af9ca33e6921b789009d12667d
LizenzFass78851 <82592556+LizenzFass78851@users.noreply.github.com>
2025-11-18 17:25:18 +0100

    Update Version-Nr. (own adjustment)
    from https://git.uupdump.net/uup-dump/api
    commit https://git.uupdump.net/uup-dump/api/commit/6b5a6885f8e8f8b3080a4b058f1f9f4b68a38b37

--- shared/main.php
+++ shared/main.php
@@ -16,7 +16,7 @@ limitations under the License.
 */
 
 function uupApiVersion() {
-    return "I can't be bothered to update the version; use commit hash.";
+    return '6b5a6885f8-dev';
 }
 
 require_once dirname(__FILE__).'/auths.php';
